5e366135ab6fc621bb0c4c004e0c817f1d99ec5e
dwalls <dan_walls@optum.com>
2022-02-09 09:05:54 -0500

    project

--- project/warframe.go
+++ project/warframe.go
@@ -5,9 +5,35 @@ import (
 	"os"
 	"os/exec"
 	"time"
+	"encoding/json"
+    "io/ioutil"
+	// "strconv"
 )
 
+// Begin Global Structures ------------------------------------------------------------
+// Main array of all the warframes Users
+type Users struct {
+    Users []User `json:"users"`
+}
 
+type User struct {
+    Gamertag   string `json:"gamertag"`
+    Warframe   string `json:"warframe"`
+    Primary    string `json:"primary"`
+	Secondary  string `json:"secondary"`
+	Melee      string `json:"melee"`
+    Companion Companion `json:"companion"`
+}
+
+type Companion struct {
+    Name    string `json:"name"`
+    Weapon  string `json:"weapon"`
+}
+
+// End Global Structures ------------------------------------------------------------
+
+
+// Begin Functions ------------------------------------------------------------
 // Clears screen with no return
 func clearScreen() {
 
@@ -36,10 +62,30 @@ func DisplayMainMenu() string {
 	return menuChoice
 }
 
+// End Functions ------------------------------------------------------------
+
 // Read in json file and display all data within that file - no return for now
 func DisplayAllData() {
 
+	jsonFile, err := os.Open("wf_data.json")
+	if err != nil { 
+		fmt.Println(err) 
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var users Users
+
+	json.Unmarshal(byteValue, &users)
+
+	for i := 0; i < len(users.Users); i++ {
+        fmt.Println(users.Users[i])
+    }
+
 }
+// End Functions ------------------------------------------------------------
+
 
 // Main kiosk processing loop
 func main(){
